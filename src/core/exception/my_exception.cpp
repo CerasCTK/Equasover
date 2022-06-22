2ff4e79d5d9a9f21b7426b429876a2810cdaa7a2
CerasCTK <ceras410@gmail.com>
2022-06-22 10:36:34 +0700

    Fixed what() method don't show the error message

--- src/core/exception/my_exception.cpp
+++ src/core/exception/my_exception.cpp
@@ -3,6 +3,8 @@
 #include "../../util/string_helper/string_helper.h"
 
 my_exception::my_exception(const char* error_message) {
+    const int message_length = util::string_helper::strlen(error_message);
+    this->error_message = new char[message_length];
     util::string_helper::strcpy(this->error_message, error_message);
 }
 
