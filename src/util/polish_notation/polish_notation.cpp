608b3ffe8823dd2e900a5158412fa8e33f49aa90
CerasCTK <ceras410@gmail.com>
2022-06-22 19:15:27 +0700

    Added some logic for polish_notation

--- src/util/polish_notation/polish_notation.cpp
+++ src/util/polish_notation/polish_notation.cpp
@@ -1,4 +1,36 @@
 #include "polish_notation.h"
 
+#include "../../core/exception/my_exception.h"
+
+#include "../string_helper/string_helper.h"
+
 namespace util {
-} // util
+    polish_notation* polish_notation::get_polish() {
+        polish_notation* po_no = new polish_notation;
+
+        return po_no;
+    }
+
+    polish_notation* polish_notation::add_function(const char* function) {
+        const int function_length = string_helper::strlen(function);
+
+        this->function_string = new char[function_length + 1];
+
+        string_helper::strcpy(this->function_string, function);
+
+        return this;
+    }
+
+    polish_notation* polish_notation::init_variable_list(const char* variables) {
+        int variablesa {0};
+
+        return this;
+    }
+
+    double polish_notation::calculate() {
+        if (this->function_string == nullptr) throw my_exception("The function is null");
+        if (this->variable_list == nullptr) throw my_exception("The variable list is null");
+
+        return 0;
+    }
+}
--- src/util/polish_notation/polish_notation.h
+++ src/util/polish_notation/polish_notation.h
@@ -2,11 +2,21 @@
 #define GENETICALGORITHM_POLISH_NOTATION_H
 
 namespace util {
-
     class polish_notation {
+    private:
+        char* function_string;
+        char* variable_list;
 
+        polish_notation() = default;
+    public:
+        static polish_notation* get_polish();
+
+        polish_notation* add_function(const char* function);
+
+        polish_notation* init_variable_list(const char* variables);
+
+        double calculate();
     };
-
-} // util
+}
 
 #endif //GENETICALGORITHM_POLISH_NOTATION_H
