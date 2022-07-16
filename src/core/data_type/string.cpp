e6f4aa3a91e113c73962d913003e41dd464781b6
CerasCTK <ceras410@gmail.com>
2022-07-16 12:27:44 +0700

    Updated some functions

--- src/core/data_type/string.cpp
+++ src/core/data_type/string.cpp
@@ -9,97 +9,304 @@
 
 namespace da_ty {
     string::string() {
-        this->data = new uint8_t;
-        *this->data = END_STRING_CHAR;
+        this->str_data = new uint8_t;
+        *this->str_data = END_STRING_CHAR;
     }
 
-    string::string(const uint8_t *data) {
-        if (data == nullptr) {
-            this->data = new uint8_t;
-            *this->data = END_STRING_CHAR;
-        } else {
-            int32_t string_length{string_helper::strlen(data) + END_STRING_COUNT};
-            this->data = new uint8_t[string_length];
+    string::string(const string &str) {
+        int32_t str_data_len{string_helper::strlen(str.str_data) + END_STRING_COUNT};
 
-            string_helper::strcpy(this->data, data);
+        this->str_data = new uint8_t[str_data_len];
+
+        string_helper::strcpy(this->str_data, str.str_data);
+        *(this->str_data + str_data_len - 1) = END_STRING_CHAR;
+    }
+
+    string::string(const string &str, int32_t pos, int32_t len) {
+        if (str.str_data == nullptr) {
+            this->str_data = new uint8_t;
+            *this->str_data = END_STRING_CHAR;
+            return;
+        }
+
+        int32_t str_len{string_helper::strlen(str.str_data)};
+
+        len = len == NPOS ? str_len - pos : len;
+        len = len > str_len ? str_len : len;
+
+        int32_t str_data_len{len + END_STRING_COUNT};
+
+        this->str_data = new uint8_t[str_data_len];
+
+        for (int32_t i{0}; i < len; i++)
+            *(this->str_data + i) = *(str.str_data + pos + i);
+
+        *(this->str_data + str_data_len - 1) = END_STRING_CHAR;
+    }
+
+    string::string(const uint8_t *s) {
+        if (s == nullptr) {
+            this->str_data = new uint8_t;
+            *this->str_data = END_STRING_CHAR;
+        } else {
+            int32_t str_data_len{string_helper::strlen(s) + END_STRING_COUNT};
+            this->str_data = new uint8_t[str_data_len];
+
+            string_helper::strcpy(this->str_data, s);
+            *(this->str_data + str_data_len - 1) = END_STRING_CHAR;
         }
     }
 
-    string::string(const string &other) {
-        int32_t string_length{string_helper::strlen(other.data) + END_STRING_COUNT};
+    string::string(const uint8_t *s, int32_t len) {
+        if (s == nullptr) {
+            this->str_data = new uint8_t;
+            *this->str_data = END_STRING_CHAR;
+        } else {
+            len = len > string_helper::strlen(s) ? string_helper::strlen(s) : len;
 
-        this->data = new uint8_t[string_length];
+            int32_t str_data_len{len + END_STRING_COUNT};
 
-        string_helper::strcpy(this->data, other.data);
-        *(this->data + string_length - 1) = END_STRING_CHAR;
+            this->str_data = new uint8_t[str_data_len];
+
+            for (int32_t i{0}; i < len; i++)
+                *(this->str_data + i) = *(s + i);
+
+            *(this->str_data + str_data_len - 1) = END_STRING_CHAR;
+        }
     }
 
-    string::string(string &&other) {
-        this->data = other.data;
-        other.data = nullptr;
+    string::string(uint32_t n, uint8_t c) {
+        // TODO: Fills the string with n consecutive copies of character c.
+        uint32_t str_data_len{n + END_STRING_COUNT};
+
+        for (int32_t i{0}; i < n; i++)
+            *(this->str_data + i) = c;
+
+        *(this->str_data + str_data_len - 1) = END_STRING_CHAR;
+    }
+
+    string::string(string &&str) noexcept {
+        this->str_data = str.str_data;
+        str.str_data = nullptr;
     }
 
     string::~string() {
-        if (this->data != nullptr)
-            delete this->data;
+        if (this->str_data != nullptr)
+            delete this->str_data;
     }
 
-    int string::length() {
-        return string_helper::strlen(this->data);
-    }
-
-    uint8_t *string::c_str() {
-        return this->data;
-    }
-
-    std::ostream &operator<<(std::ostream &os, const string &obj) {
-        os << obj.data;
-        return os;
-    }
-
-    std::istream &operator>>(std::istream &is, string &obj) {
-        uint8_t *buff{new uint8_t[MAX_STRING_LENGTH_DEFAULT]};
-        memset(&buff[0], 0, sizeof(buff));
-        is >> reinterpret_cast<bool &>(buff);
-        obj = string{buff};
-        delete[] buff;
-        return is;
-    }
-
-    string &string::operator=(const string &other) {
-        if (this == &other)
+    string &string::operator=(const string &str) {
+        if (this == &str)
             return *this;
 
-        delete this->data;
-        int32_t string_length{string_helper::strlen(other.data) + END_STRING_COUNT};
-        this->data = new uint8_t[string_length];
-        string_helper::strcpy(this->data, other.data);
+        delete this->str_data;
+        int32_t string_length{string_helper::strlen(str.str_data) + END_STRING_COUNT};
+        this->str_data = new uint8_t[string_length];
+        string_helper::strcpy(this->str_data, str.str_data);
         return *this;
     }
 
-    string &string::operator=(const uint8_t *text) {
-        if (string_helper::strcmp(this->data, text))
+    string &string::operator=(const uint8_t *s) {
+        if (string_helper::strcmp(this->str_data, s))
             return *this;
 
-        delete this->data;
+        delete this->str_data;
 
-        int32_t string_length{string_helper::strlen(text) + END_STRING_COUNT};
-        this->data = new uint8_t[string_length];
-        string_helper::strcpy(this->data, text);
+        int32_t string_length{string_helper::strlen(s) + END_STRING_COUNT};
+        this->str_data = new uint8_t[string_length];
+        string_helper::strcpy(this->str_data, s);
         return *this;
+    }
+
+    string &string::operator=(uint8_t c) {
+        if (this->str_data != nullptr)
+            delete this->str_data;
+
+        this->str_data = new uint8_t[1 + END_STRING_COUNT];
+        *this->str_data = c;
+        *(this->str_data + 1) = END_STRING_CHAR;
+
+        return *this;
+    }
+
+    string &string::operator=(string &&str) noexcept {
+        this->str_data = str.str_data;
+        str.str_data = nullptr;
+
+        return *this;
+    }
+
+    int32_t string::size() const noexcept {
+        return string_helper::strlen(this->str_data);
+    }
+
+    int32_t string::length() const noexcept {
+        return string_helper::strlen(this->str_data);
+    }
+
+    int32_t string::max_size() const noexcept {
+        return MAX_STRING_LENGTH;
+    }
+
+    void string::resize(int32_t n) {
+        // TODO
+    }
+
+    void string::resize(int32_t n, uint8_t c) {
+        // TODO
+    }
+
+    void string::clear() noexcept {
+        // TODO
+    }
+
+    bool string::empty() const noexcept {
+        // TODO
+    }
+
+    uint8_t &string::operator[](int32_t pos) {
+        // TODO
+    }
+
+    const uint8_t &string::operator[](int32_t pos) const {
+        if (pos >= this->length())
+            throw my_exception((uint8_t *) ("Index out of bound"));
+        return *(this->str_data + pos);
+    }
+
+    uint8_t &string::at(int32_t index) {
+        if (index >= this->length())
+            throw my_exception((uint8_t *) ("Index out of bound"));
+        return *(this->str_data + index);
+    }
+
+    const uint8_t &string::at(int32_t index) const {
+        if (index >= this->length())
+            throw my_exception((uint8_t *) ("Index out of bound"));
+        return *(this->str_data + index);
+    }
+
+    uint8_t &string::back() {
+        // TODO:
+    }
+
+    const uint8_t &string::back() const {
+        // TODO
+    }
+
+    uint8_t &string::front() {
+        // TODO
+    }
+
+    const uint8_t &string::front() const {
+        // TODO
+    }
+
+    string &string::operator+=(const string &str) {
+        // TODO:
+    }
+
+    string &string::operator+=(const uint8_t *s) {
+        // TODO
+    }
+
+    string &string::operator+=(uint8_t c) {
+        // TODO
+    }
+
+    string &string::append(const string &str) {
+        // TODO
+    }
+
+    string &string::append(const string &str, int32_t sub_pos, int32_t sub_len) {
+        // TODO
+    }
+
+    string &string::append(const uint8_t *s) {
+        // TODO
+    }
+
+    string &string::append(const uint8_t *s, size_t n) {
+        // TODO
+    }
+
+    string &string::append(int32_t n, uint8_t c) {
+        // TODO
+    }
+
+    void string::push_back(uint8_t c) {
+        int32_t new_length{string_helper::strlen(this->str_data) + 1 + END_STRING_COUNT};
+
+        uint8_t *buff{new uint8_t[new_length]};
+        string_helper::strcpy(buff, this->str_data);
+        buff[new_length - 2] = c;
+        buff[new_length - 1] = END_STRING_CHAR;
+
+        delete this->str_data;
+
+        this->str_data = buff;
+    }
+
+    string &string::erase(int32_t pos, int32_t len) {
+        // TODO
+    }
+
+    string &string::replace(int32_t pos, int32_t len, const string &str, int32_t sub_pos, int32_t sub_len) {
+        // TODO
+    }
+
+    void string::swap(string &str) {
+        // TODO:
+    }
+
+    void string::pop_back() {
+        // TODO:
+    }
+
+    const uint8_t *string::c_str() const noexcept {
+        return this->str_data;
+    }
+
+    const uint8_t *string::data() const noexcept {
+        // TODO
+    }
+
+    int32_t string::copy(uint8_t *s, int32_t len, int32_t pos) const {
+        // TODO
+    }
+
+    int32_t string::find(const string &str, int32_t pos) const {
+        // TODO
+    }
+
+    int32_t string::find(const uint8_t *s, int32_t pos) const {
+        // TODO
+    }
+
+    int32_t string::find(const uint8_t *s, int32_t pos, int32_t n) const {
+        // TODO
+    }
+
+    int32_t string::find(uint8_t c, int32_t pos) const {
+        // TODO
+    }
+
+    string string::substr(int32_t pos, int32_t len) const {
+        // TODO
     }
 
     string operator+(const string &left, const string &right) {
-        int32_t new_length{string_helper::strlen(left.data) + string_helper::strlen(right.data) + END_STRING_COUNT};
+        int32_t new_length{
+                string_helper::strlen(left.str_data) + string_helper::strlen(right.str_data) + END_STRING_COUNT};
 
         uint8_t *buff{new uint8_t[new_length]};
 
-        string_helper::strcpy(buff, left.data);
-        string_helper::strcat(buff, right.data);
+        string_helper::strcpy(buff, left.str_data);
+        string_helper::strcat(buff, right.str_data);
 
         buff[new_length - 1] = END_STRING_CHAR;
 
-        string temp {buff};
+        string temp{buff};
 
         delete[] buff;
 
@@ -107,58 +314,141 @@ namespace da_ty {
     }
 
     string operator+(const string &left, const uint8_t *text) {
-        int32_t new_length{string_helper::strlen(left.data) + string_helper::strlen(text) + END_STRING_COUNT};
+        int32_t new_length{string_helper::strlen(left.str_data) + string_helper::strlen(text) + END_STRING_COUNT};
 
         uint8_t *buff{new uint8_t[new_length]};
 
-        string_helper::strcpy(buff, left.data);
+        string_helper::strcpy(buff, left.str_data);
         string_helper::strcat(buff, text);
 
         buff[new_length - 1] = END_STRING_CHAR;
 
-        string temp {buff};
+        string temp{buff};
 
         delete[] buff;
 
         return temp;
     }
 
-    bool string::operator==(string other) {
-        return string_helper::strcmp(this->data, other.data);
+    string operator+(const uint8_t *lhs, const string &rhs) {
+        // TODO
     }
 
-    bool string::operator==(const uint8_t *text) {
-        return string_helper::strcmp(this->data, text);
+    string operator+(const string &lhs, uint8_t rhs) {
+        // TODO
     }
+
+    string operator+(uint8_t lhs, const string &rhs) {
+        // TODO
+    }
+
+    bool operator==(const string &lhs, const string &rhs) noexcept {
+        // TODO
+    }
+
+    bool operator==(const uint8_t *lhs, const string &rhs) {
+        // TODO
+    }
+
+    bool operator==(const string &lhs, const uint8_t *rhs) {
+        // TODO
+    }
+
+    bool operator!=(const string &lhs, const string &rhs) noexcept {
+        // TODO
+    }
+
+    bool operator!=(const uint8_t *lhs, const string &rhs) {
+        // TODO
+    }
+
+    bool operator!=(const string &lhs, const uint8_t *rhs) {
+        // TODO
+    }
+
+    bool operator<(const string &lhs, const string &rhs) noexcept {
+        // TODO
+    }
+
+    bool operator<(const uint8_t *lhs, const string &rhs) {
+        // TODO
+    }
+
+    bool operator<(const string &lhs, const uint8_t *rhs) {
+        // TODO
+    }
+
+    bool operator<=(const string &lhs, const string &rhs) noexcept {
+        // TODO
+    }
+
+    bool operator<=(const uint8_t *lhs, const string &rhs) {
+        // TODO
+    }
+
+    bool operator<=(const string &lhs, const uint8_t *rhs) {
+        // TODO
+    }
+
+    bool operator>(const string &lhs, const string &rhs) noexcept {
+        // TODO
+    }
+
+    bool operator>(const uint8_t *lhs, const string &rhs) {
+        // TODO
+    }
+
+    bool operator>(const string &lhs, const uint8_t *rhs) {
+        // TODO
+    }
+
+    bool operator>=(const string &lhs, const string &rhs) noexcept {
+        // TODO
+    }
+
+    bool operator>=(const uint8_t *lhs, const string &rhs) {
+        // TODO
+    }
+
+    bool operator>=(const string &lhs, const uint8_t *rhs) {
+        // TODO
+    }
+
+
+    void string::get_line() {
+        if (this->str_data != nullptr)
+            delete this->str_data;
+
+        int32_t str_data_len{MAX_STRING_LENGTH_DEFAULT};
+
+        this->str_data = new uint8_t[str_data_len];
+        string_helper::get_text(this->str_data, str_data_len);
+    }
+
+    std::ostream &operator<<(std::ostream &os, const string &obj) {
+        os << obj.str_data;
+        return os;
+    }
+
+//    std::istream &operator>>(std::istream &is, string &obj) {
+//        uint8_t *buff{new uint8_t[MAX_STRING_LENGTH_DEFAULT]};
+//        memset(&buff[0], 0, sizeof(buff));
+//        is >> reinterpret_cast<short &>(buff);
+//        obj = string{buff};
+//        string_helper::strcpy(buff, obj.str_data);
+//        delete[] buff;
+//        return is;
+//    }
 
     da_st::array_list<string> *string::split(const uint8_t split_char) {
         using namespace da_st;
         array_list<string> *list = new array_list<string>;
 
-        while (*(this->data) != '\0') {
+        while (*(this->str_data) != '\0') {
 
         }
 
         return list;
-    }
-
-    uint8_t &string::at(int32_t index) {
-        if (index >= this->length())
-            throw my_exception((uint8_t *) ("Index out of bound"));
-        return *(this->data + index);
-    }
-
-    void string::push_back(uint8_t c) {
-        int32_t new_length{string_helper::strlen(this->data) + 1 + END_STRING_COUNT};
-
-        uint8_t *buff{new uint8_t[new_length]};
-        string_helper::strcpy(buff, this->data);
-        buff[new_length - 2] = c;
-        buff[new_length - 1] = END_STRING_CHAR;
-
-        delete this->data;
-
-        this->data = buff;
     }
 
     string string::clone() {
--- src/core/data_type/string.h
+++ src/core/data_type/string.h
@@ -13,49 +13,168 @@
 
 #define MAX_STRING_LENGTH_DEFAULT 1000
 
-#define END_STRING_COUNT 1 // \0
+#define MAX_STRING_LENGTH UINT32_MAX
+
 #define END_STRING_CHAR '\0'
+#define END_STRING_COUNT 1 // \0
+
+#define NPOS -1
 
 namespace da_ty {
-class string : public cloneable<string> {
+    class string : public cloneable<string> {
     private:
-        uint8_t *data;
+        uint8_t *str_data;
     public:
         string();
 
-        string(const uint8_t *data);
+        string(const string &str);
 
-        string(const string &other);
+        string(const string &str, int32_t pos, int32_t len = NPOS);
 
-        string(string &&other);
+        string(const uint8_t *s);
+
+        string(const uint8_t *s, int32_t len);
+
+        string(uint32_t n, uint8_t c);
+
+        string(string &&str) noexcept;
 
         ~string();
 
-        int32_t length();
+        string &operator=(const string &str);
 
-        uint8_t *c_str();
+        string &operator=(const uint8_t *s);
+
+        string &operator=(uint8_t c);
+
+        string &operator=(string &&str) noexcept;
+
+        int32_t size() const noexcept;
+
+        int32_t length() const noexcept;
+
+        int32_t max_size() const noexcept;
+
+        void resize(int32_t n);
+
+        void resize(int32_t n, uint8_t c);
+
+        void clear() noexcept;
+
+        bool empty() const noexcept;
+
+        uint8_t &operator[](int32_t pos);
+
+        const uint8_t &operator[](int32_t pos) const;
 
         uint8_t &at(int32_t index);
 
+        const uint8_t &at(int32_t index) const;
+
+        uint8_t &back();
+
+        const uint8_t &back() const;
+
+        uint8_t &front();
+
+        const uint8_t &front() const;
+
+        string &operator+=(const string &str);
+
+        string &operator+=(const uint8_t *s);
+
+        string &operator+=(uint8_t c);
+
+        string &append(const string &str);
+
+        string &append(const string &str, int32_t sub_pos, int32_t sub_len);
+
+        string &append(const uint8_t *s);
+
+        string &append(const uint8_t *s, size_t n);
+
+        string &append(int32_t n, uint8_t c);
+
         void push_back(uint8_t c);
 
-        da_st::array_list<string> *split(const uint8_t split_char);
+        string &erase(int32_t pos = 0, int32_t len = NPOS);
+
+        string &replace(int32_t pos, int32_t len, const string &str, int32_t sub_pos, int32_t sub_len);
+
+        void swap(string &str);
+
+        void pop_back();
+
+        const uint8_t *c_str() const noexcept;
+
+        const uint8_t *data() const noexcept;
+
+        int32_t copy(uint8_t *s, int32_t len, int32_t pos = 0) const;
+
+        int32_t find(const string &str, int32_t pos = 0) const;
+
+        int32_t find(const uint8_t *s, int32_t pos = 0) const;
+
+        int32_t find(const uint8_t *s, int32_t pos, int32_t n) const;
+
+        int32_t find(uint8_t c, int32_t pos = 0) const;
+
+        string substr(int32_t pos = 0, int32_t len = NPOS) const;
+
+        friend string operator+(const string &lhs, const string &rhs);
+
+        friend string operator+(const string &lhs, const uint8_t *rhs);
+
+        friend string operator+(const uint8_t *lhs, const string &rhs);
+
+        friend string operator+(const string &lhs, uint8_t rhs);
+
+        friend string operator+(uint8_t lhs, const string &rhs);
+
+        friend bool operator==(const string &lhs, const string &rhs) noexcept;
+
+        friend bool operator==(const uint8_t *lhs, const string &rhs);
+
+        friend bool operator==(const string &lhs, const uint8_t *rhs);
+
+        friend bool operator!=(const string &lhs, const string &rhs) noexcept;
+
+        friend bool operator!=(const uint8_t *lhs, const string &rhs);
+
+        friend bool operator!=(const string &lhs, const uint8_t *rhs);
+
+        friend bool operator<(const string &lhs, const string &rhs) noexcept;
+
+        friend bool operator<(const uint8_t *lhs, const string &rhs);
+
+        friend bool operator<(const string &lhs, const uint8_t *rhs);
+
+        friend bool operator<=(const string &lhs, const string &rhs) noexcept;
+
+        friend bool operator<=(const uint8_t *lhs, const string &rhs);
+
+        friend bool operator<=(const string &lhs, const uint8_t *rhs);
+
+        friend bool operator>(const string &lhs, const string &rhs) noexcept;
+
+        friend bool operator>(const uint8_t *lhs, const string &rhs);
+
+        friend bool operator>(const string &lhs, const uint8_t *rhs);
+
+        friend bool operator>=(const string &lhs, const string &rhs) noexcept;
+
+        friend bool operator>=(const uint8_t *lhs, const string &rhs);
+
+        friend bool operator>=(const string &lhs, const uint8_t *rhs);
+
+
+        void get_line();
 
         friend std::ostream &operator<<(std::ostream &os, const string &obj);
 
-        friend std::istream &operator>>(std::istream &is, string &obj);
+//        friend std::istream &operator>>(std::istream &is, string &obj);
 
-        string &operator=(const string &other);
-
-        string &operator=(const uint8_t *text);
-
-        friend string operator+(const string &left, const string &right);
-
-        friend string operator+(const string &left, const uint8_t *text);
-
-        bool operator==(string other);
-
-        bool operator==(const uint8_t *text);
+        da_st::array_list<string> *split(const uint8_t split_char);
 
         string clone();
     };
